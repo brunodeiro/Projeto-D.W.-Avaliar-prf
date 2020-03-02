4646e03adf54bf6e3e93b40dca4d174a8ae65c76
brunodeiro <bruno_deiro@live.com>
2020-03-02 00:34:04 -0300

    modelo base do projeto

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+var header = document.getElementById("header")
+var menu = document.getElementById("dracula")
+var footer = document.getElementById("footer")
+
+var divHeaderEl = document.createElement("div")
+divHeaderEl.id="DivHeader"
+header.appendChild(divHeaderEl)
+
+var divFooter = document.createElement("div")
+divFooter.id="divFooter"
+footer.appendChild(divFooter)
+
+var divFormEl = document.createElement("div")
+divFormEl.id="divForm"
+menu.appendChild(divFormEl)
+
+var formEl = document.createElement("form")
+formEl.setAttribute("action", "formulario.php")
+formEl.setAttribute("method", "post")
+formEl.id="form"
+divFormEl.appendChild(formEl)
+
