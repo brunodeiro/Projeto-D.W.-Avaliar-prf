4847e9b084dbf2b917525eb9db889f4dcff1d57b
SimonMarce <irvingcesar22@gmail.com>
2020-03-05 11:03:44 -0300

    Centralizando a tela

--- 02RecuperarSenha/JS/recuperar.js
+++ 02RecuperarSenha/JS/recuperar.js
@@ -1,1 +1,7 @@
-
+function login(){
+    window.location.href = "index.html"
+  }
+  function redirecionar(){
+      alert("Solicitação enviada! Verifique seu E-mail.")
+      window.location.href = "index.html"
+  }
