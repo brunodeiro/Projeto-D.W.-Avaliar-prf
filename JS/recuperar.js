08bdcd8a7ddfeb47c2cc6920774d9372c29f0dd3
brunodeiro <bruno_deiro@live.com>
2020-03-07 01:18:05 -0300

    att

--- JS/recuperar.js
+++ JS/recuperar.js
@@ -1,7 +1,17 @@
 function login(){
     window.location.href = "index.html"
-  }
-  function redirecionar(){
-      alert("Solicitação enviada! Verifique seu E-mail.")
-      window.location.href = "index.html"
-  }
+}
+function validar(){
+    let inputUser = document.getElementById('inputusuario')
+    let inputEmail = document.getElementById('inputemail')
+
+    if(inputUser.value != '' && inputEmail != ''){
+        alert("Solicitação enviada! Verifique seu E-mail.")
+        window.location.href = "index.html"
+    }else{
+        alert('Insira o usuário e E-mail nos campos abaixo!')
+    }
+}
+
+
+
