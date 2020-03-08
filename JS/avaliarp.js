55f1d94ee4ceb3008aa46731127bce5da738270e
brunodeiro <bruno_deiro@live.com>
2020-03-08 11:31:23 -0300

    inclusão de novos elementos

--- JS/avaliarp.js
+++ JS/avaliarp.js
@@ -3,24 +3,43 @@ var menu = document.getElementById("dracula")
 var footer = document.getElementById("footer")
 
 var divHeaderEl = document.createElement("div")
-divHeaderEl.id="DivHeader"
-header.appendChild(divHeaderEl)
+    divHeaderEl.id="DivHeader"
+    header.appendChild(divHeaderEl)
 
 var divFooter = document.createElement("div")
-divFooter.id="divFooter"
-footer.appendChild(divFooter)
+    divFooter.id="divFooter"
+    footer.appendChild(divFooter)
 
 var divFormEl = document.createElement("div")
-divFormEl.id="divForm"
-menu.appendChild(divFormEl)
+    divFormEl.id="divForm"
+    menu.appendChild(divFormEl)
 
 var logoEl = document.createElement("img")
-logoEl.id="logo"
-logoEl.src="IMG/sesi-logo-1.png"
-logoEl.setAttribute("width", "200px")
-logoEl.setAttribute("height", "80px")
-divHeaderEl.appendChild(logoEl)
+    logoEl.id="logo"
+    logoEl.src="IMG/sesi-logo-1.png"
+    logoEl.setAttribute("width", "200px")
+    logoEl.setAttribute("height", "80px")
+    divHeaderEl.appendChild(logoEl)
 
 var divFiltro = document.createElement("div")
-divFiltro.id="div_filtro"
-divFormEl.appendChild(divFormEl)
+    divFiltro.id="div_filtro"
+    divFormEl.appendChild(divFormEl)
+
+var labelFiltro = document.createElement("label")
+    labelFiltro.id="labelFiltro"
+    labelFiltro.innerText="Filtrar por:"
+    divFiltro.appendChild(labelFiltro)
+
+var divBtn = document.createElement("div")
+    divBtn.id="divBtn"
+    divFiltro.appendChild(divBtn)
+
+var avaliarP = document.createElement("button")
+    avaliarP.id="avaliarP"
+    avaliarP.innerText="Professor"
+    divBtn.appendChild(avaliarP)
+
+var avaliarM = document.createElement("button")
+    avaliarM.id="avaliarM"
+    avaliarM.innerText="Matéria"
+    divBtn.appendChild(avaliarM)    
