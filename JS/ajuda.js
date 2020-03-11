3af986a9a78d07e025ab7ccb939d3adcc518a368
brunodeiro <bruno_deiro@live.com>
2020-03-11 14:52:27 -0300

    att

---
+++ JS/ajuda.js
@@ -0,0 +1,50 @@
+var header = document.getElementById("header")
+var menu = document.getElementById("dracula")
+var footer = document.getElementById("footer")
+
+var divHeaderEl = document.createElement("div")
+    divHeaderEl.id="DivHeader"
+    header.appendChild(divHeaderEl)
+
+var divFooter = document.createElement("div")
+    divFooter.id="divFooter"
+    footer.appendChild(divFooter)
+
+var divFormEl = document.createElement("div")
+    divFormEl.id="divForm"
+    menu.appendChild(divFormEl)
+
+var logoEl = document.createElement("img")
+    logoEl.id="logo"
+    logoEl.src="IMG/sesi-logo-1.png"
+    logoEl.setAttribute("width", "200px")
+    logoEl.setAttribute("height", "80px")
+    divHeaderEl.appendChild(logoEl)
+
+var h2El = document.createElement("h2")
+    h2El.id="h2El"
+    h2El.innerText="Documentação"
+    divFormEl.appendChild(h2El)
+
+var br = document.createElement("br")
+divFormEl.appendChild(br)
+
+var txt = document.createElement("p")
+    txt.id="txt"
+    txt.innerText=""
+    divFormEl.appendChild(txt)
+
+var br1 = document.createElement("br")
+    divFormEl.appendChild(br1)
+
+var br2 = document.createElement("br")
+    br2.innerText=""
+    divFormEl.appendChild(br2)
+
+var br3 = document.createElement("br")
+    divFormEl.appendChild(br3)
+
+var txt2 = document.createElement("p")
+    txt2.id="txt2"
+    txt2.innerText="Mussum Ipsum, cacilds vidis litro abertis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Casamentiss faiz malandris se pirulitá. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Paisis, filhis, espiritis santis. Per aumento de cachacis, eu reclamis. Atirei o pau no gatis, per gatis num morreus. Quem num gosta di mé, boa gentis num é. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Leite de capivaris, leite de mula manquis sem cabeça. Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Pra lá , depois divoltis porris, paradis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Delegadis gente finis, bibendum egestas augue arcu ut est. Aenean aliquam molestie leo, vitae iaculis nisl. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. A ordem dos tratores não altera o pão duris. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Viva Forevis aptent taciti sociosqu ad litora torquent. Si num tem leite então bota uma pinga aí cumpadi! In elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Quem num gosta di mim que vai caçá sua turmis! Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Interagi no mé, cursus quis, vehicula ac nisi. Detraxit consequat et quo num tendi nada. Suco de cevadiss deixa as pessoas mais interessantis. Cevadis im ampola pa arma uma pindureta. Diuretics paradis num copo é motivis de denguis. Mé faiz elementum girarzis, nisi eros vermeio. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Quem manda na minha terra sou euzis! Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Sapien in monti palavris qui num significa nadis i pareci latim. Per aumento de cachacis, eu reclamis. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Delegadis gente finis, bibendum egestas augue arcu ut est. Diuretics paradis num copo é motivis de denguis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Atirei o pau no gatis, per gatis num morreus. Paisis, filhis, espiritis santis. Suco de cevadiss deixa as pessoas mais interessantis.Quem num gosta di mim que vai caçá sua turmis! Detraxit consequat et quo num tendi nada. Cevadis im ampola pa arma uma pindureta. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum."
+    divFormEl.appendChild(txt2)
