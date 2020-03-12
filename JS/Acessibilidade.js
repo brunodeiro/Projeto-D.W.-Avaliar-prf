2a1884b33ff8f9e3b93ae95ff16bde457623f063
SimonMarce <irvingcesar22@gmail.com>
2020-03-11 23:47:28 -0300

    Modificando Recuperar.html

---
+++ JS/Acessibilidade.js
@@ -0,0 +1,57 @@
+window.onload = function() {
+    let bodyEl = document.querySelector('body');
+    let botaoAumentar = document.getElementById('aumentar');
+    let botaoDiminuir = document.getElementById('diminuir');
+
+    //Fonte notmal do body
+    let fontSize = 100;
+
+    let aumentarDiminuir = 10;
+
+    botaoAumentar.addEventListener('click', function(event) {
+        fontSize += aumentarDiminuir;
+        bodyEl.style.fontSize = fontSize + '%';
+    });
+
+    botaoDiminuir.addEventListener('click', function(event) {
+        fontSize -= aumentarDiminuir;
+        bodyEl.style.fontSize = fontSize + '%';
+    });
+}
+
+
+function escuro(){
+    let img1 = document.getElementsByClassName('alto-contraste')
+    let bodyEl = document.querySelector('body')
+    let titulo1 = document.getElementById('titulo')
+    if(img1){
+        bodyEl.className = 'fundo1'
+        titulo1.style.color = 'white'
+    }
+}
+function branco(){
+    let img2 = document.getElementsByClassName('contraste-normal')
+    let bodyEl = document.querySelector('body')
+    let titulo2 = document.getElementById('titulo')
+    if(img2){
+        titulo2.style.color = 'black'
+        bodyEl.className = 'fundo'
+    }
+}
+   
+
+
+
+
+// function mudarContraste(contraste) {
+//     let bodyEl = document.querySelector('body')
+//     let titulo = document.getElementById('titulo')
+//     if(contraste.value === 'branco'){
+//         titulo.style.color = 'white'
+//         bodyEl.className = 'fundo1'
+
+//     }else if(contraste.value === 'escuro'){
+//         titulo.style.color = 'black'
+//         bodyEl.className = 'fundo'
+//     }
+// }
