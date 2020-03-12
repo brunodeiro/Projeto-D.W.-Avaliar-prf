7c4761d1576dc97ac781b9fc24e7b2751ff9182c
SimonMarce <irvingcesar22@gmail.com>
2020-03-12 09:15:38 -0300

    Adicionando acessibilidade ao index.html

--- JS/Acessibilidade.js
+++ JS/Acessibilidade.js
@@ -42,6 +42,7 @@ function branco(){
         bodyEl.className = 'fundo'
     }
 }
+
    
 
 
--- JS/script.js
+++ JS/script.js
@@ -124,3 +124,53 @@ usersenha='admin'
 //     checkboxEl.checked=true
 //     //CHECKBOX É MARCADO AO CLICAR NO "MANTER LOGADO"
 // }    
+
+
+
+
+// Deixando funções de acessibilidade nessa parte por conta de um erro
+
+window.onload = function() {
+    let bodyEl = document.querySelector('body');
+    let botaoAumentar = document.getElementById('aumentar');
+    let botaoDiminuir = document.getElementById('diminuir');
+
+    //Fonte normal do body
+    let fontSize = 100;
+
+    let aumentarDiminuir = 15;
+
+    botaoAumentar.addEventListener('click', function(event) {
+        if(fontSize<115){
+            fontSize += aumentarDiminuir;
+            bodyEl.style.fontSize = fontSize + '%';
+        }
+    });
+
+    botaoDiminuir.addEventListener('click', function(event) {
+        if(fontSize>100){
+            fontSize -= aumentarDiminuir;
+            bodyEl.style.fontSize = fontSize + '%';
+        }
+    });
+}
+
+
+function escuro(){
+    let img1 = document.getElementsByClassName('alto-contraste')
+    let bodyEl = document.querySelector('body')
+    let titulo1 = document.getElementById('titulo')
+    if(img1){
+        bodyEl.className = 'fundo1'
+        titulo1.style.color = 'white'
+    }
+}
+function branco(){
+    let img2 = document.getElementsByClassName('contraste-normal')
+    let bodyEl = document.querySelector('body')
+    let titulo2 = document.getElementById('titulo')
+    if(img2){
+        // titulo2.style.color = 'black'  //O Erro
+        bodyEl.className = 'fundo'
+    }
+}
