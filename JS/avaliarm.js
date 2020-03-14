88a3945e6ef81bd4e46710510a22d1e22e0eb989
SimonMarce <irvingcesar22@gmail.com>
2020-03-14 00:24:00 -0300

    Alterações feitas

--- JS/avaliarm.js
+++ JS/avaliarm.js
@@ -15,12 +15,12 @@ var divFormEl = document.createElement("div")
     divFormEl.id="divForm"
     menu.appendChild(divFormEl)
 
-var logoEl = document.createElement("img")
-    logoEl.id="logo"
-    logoEl.src="IMG/sesi-logo-1.png"
-    logoEl.setAttribute("width", "200px")
-    logoEl.setAttribute("height", "80px")
-    divHeaderEl.appendChild(logoEl)
+// var logoEl = document.createElement("img")
+//     logoEl.id="logo"
+//     logoEl.src="IMG/sesi-logo-1.png"
+//     logoEl.setAttribute("width", "200px")
+//     logoEl.setAttribute("height", "80px")
+//     divHeaderEl.appendChild(logoEl)
 
 var divFiltro = document.createElement("div")
     divFiltro.id="divFiltro"
